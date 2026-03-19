5e5274b439094030073a08d112dccc1a0d9d1038
지민 최 <jimin_0128@daum.net>
2026-03-19 09:10:54 +0900

    과제1 코딩과 테스트 완료 1) UI 구성 및 기능 설명
    폼에 아래 컨트롤을 배치함.
    ListBox를 중앙에 TextBox를 ListBox 아래에 배치
    전송 버튼을 누르면 TextBox에 입력한 내용이 ListBox로 들어가 표시되게 하고 TextBox의 내용은 깨끗하게 지워지게 한다.
    
    2) 초기 컨트롤 이름 설정
    
    TextBox : 사용자 메시지 입력
    
    Button : 메시지 전송
    
    ListBox : 전송된 메시지 표시
    
    3 컨트롤 이름 설정
    
    TextBox → txtMessage
    
    Button → btnSend
    
    ListBox → lstChat
    
    5) 설명
    
    txtMessage.Text : 입력창의 문자열을 가져온다.
    
    lstChat.Items.Add(msg) : 가져온 문자열을 ListBox에 추가한다
    
    txtMessage.Clear() : 입력창 비우기
    
    등을 추가함
    
    Echo Messenger UI 및 메시지 전송 기능 추가
    
    Echo Messenger 채팅 프로그램의 WinForms UI를 구성하고, 메시지 입력(TextBox), 전송 버튼(Button), 채팅 내역(ListBox) 컨트롤을 추가했습니다. 전송 버튼 클릭 시 입력한 메시지가 채팅창에 표시되는 기능을 구현했습니다. 또한 각 컨트롤의 디자인과 이벤트 핸들러를 설정하였습니다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            txtMessage = new TextBox();
+            btnSend = new Button();
+            lstChat = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.ForeColor = SystemColors.MenuHighlight;
+            label1.Location = new Point(55, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(258, 40);
+            label1.TabIndex = 0;
+            label1.Text = "Echo Messeenger";
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(68, 336);
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(464, 23);
+            txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = SystemColors.Info;
+            btnSend.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnSend.Location = new Point(551, 324);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(89, 45);
+            btnSend.TabIndex = 2;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = false;
+            btnSend.Click += btnSend_Click;
+            // 
+            // lstChat
+            // 
+            lstChat.FormattingEnabled = true;
+            lstChat.Location = new Point(68, 117);
+            lstChat.Name = "lstChat";
+            lstChat.Size = new Size(464, 199);
+            lstChat.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lstChat);
+            Controls.Add(btnSend);
+            Controls.Add(txtMessage);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtMessage;
+        private Button btnSend;
+        private ListBox lstChat;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,18 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // 입력값 가져오기
+            string msg = txtMessage.Text;
+
+            // ListBox에 추가
+            lstChat.Items.Add(msg);
+
+            // 입력창 비우기 (1차 과제)
+            txtMessage.Clear();
+
+        }
     }
 }
